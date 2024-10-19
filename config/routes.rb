47b68f87c6e1871060c05b05732862d1024855f6
Ashwin Almeida <almeidashwin@gmail.com>
2024-10-19 16:17:36 +1100

    watch_list_until_four

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :lists do
+    resources :bookmarks, only: [:new, :create, :delete]
+  end
+  resources :bookmarks, only: [:show, :destroy]
 end
